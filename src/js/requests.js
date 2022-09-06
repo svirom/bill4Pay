8eb7d5890e66731e71590b6718274750446ed368
svirom <svirom@yahoo.com>
2022-09-06 11:38:11 +0300

    amount round up 8 digits

--- src/js/requests.js
+++ src/js/requests.js
@@ -3,7 +3,6 @@ const getBillData = async (guid, currency) => {
   
   if (response.status === 200) {
     const data = await response.json();
-    console.log(data, parseFloat(data.amount), parseFloat(data.fee_external));
     return data;
   } else {
     throw new Error('Unable to get data');
